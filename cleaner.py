9f2b155c7c31e13e9f64597c4bc0211e4f53bcc6
Mohit <mohit.y242@gmail.com>
2018-08-20 16:39:04 +0530

    Added scripts

---
+++ cleaner.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+import re
+parser = argparse.ArgumentParser()
+parser.add_argument('imgFolder',type=str,default='./raw')
+args = parser.parse_args()
+washList = '/media/deepak/11a0ed1e-3fe3-4bfa-8528-46f0d5b76502/Mohit/MS-Celeb-1M_WashList/MsCeleb1M_washlist.txt'
+r=[]
+if os.path.exists(args.imgFolder):
+    for path, dnames, fnames in os.walk(args.imgFolder):
+        r.extend([os.path.join(path, x) for x in fnames])
+        # r.extend(fold for fold in dnames)
+# print(len(r))
+wlFile = open(washList,'r')
+goodFiles = wlFile.readlines()
+goodFiles = [f.strip('\n ') for f in goodFiles]
+print("Warning !!!!!!!!!!!!!!\nCheck folder path before deleting files: {}\nDo you want to continue?".format(args.imgFolder))
+accept= input()
+if accept.lower() != 'y':
+    print("ABORTING")
+    exit()
+print(goodFiles[:10])
+counter=0
+for imgFile in goodFiles:
+    if(os.path.exists(os.path.join(args.imgFolder,imgFile))):
+        splitPath = os.path.split(imgFile)
+        modImgFile=os.path.join(splitPath[0],'zx_'+splitPath[1])
+        os.rename(os.path.join(args.imgFolder,imgFile),os.path.join(args.imgFolder,modImgFile))
+        counter+=1
+print('Total files found-{}'.format(counter))
+counter=0
+for imgFile in r:
+    if(os.path.exists(imgFile)):
+        splitPath = os.path.split(imgFile)
+
+        if not re.match( r'zx_', splitPath[1]):
+            os.remove(imgFile)
